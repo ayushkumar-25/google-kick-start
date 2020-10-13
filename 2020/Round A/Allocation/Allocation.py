cc82243b6e0f4313d95a1a823f45ac0bd31343ea
ayushkumar25 <ayush2608@gmail.com>
2020-10-13 20:18:49 +0530

    Update Allocation.py

--- 2020/Round A/Allocation/Allocation.py
+++ 2020/Round A/Allocation/Allocation.py
@@ -1,15 +1,14 @@
-T = int(input())
-for _ in range(T):
-    sum = 0
-    N, B = [int(x) for x in input().split()]
-    l = list(map(int, input().split()))
-    l.sort()
-    for i in range(len(l)):
-        try:
-            if sum <= B:
-                sum += l[i]
-            if sum > B:
-                break
-        except Exception as e:
-            pass
-    print(f'Case #{_+1}: {i}')
+t = int(input())
+tc = 0
+while t>0:
+    n, b = map(int, input().split())
+    a = list(map(int, input().split()))
+    count = 0
+    a.sort()
+    for i in a:
+        if i<=b:
+            b -= i
+            count += 1
+    tc += 1
+    print("Case #"+str(tc)+": "+str(count))
+    t -= 1
